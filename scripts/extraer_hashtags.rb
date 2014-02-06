fc95c176ec3042dbe8a786baba2b4903aad129aa
Andres Araya <araya.andres@gmail.com>
2014-02-05 23:21:39 -0600

    Extraer hashtags como JSON.

--- scripts/extraer_hashtags.rb
+++ scripts/extraer_hashtags.rb
@@ -1,20 +1,17 @@
 require 'json'
 
-SEPARATOR = ";"
-
 file_name = ARGV[0]
 s = IO.read(file_name)
 tweets = JSON.parse(s)
 
-hashtags = {}
+tmp = {}
 tweets["statuses"].each do |t|
   t["new_hashtags"].each do |h|
-		if hashtags[h]
-			hashtags[h] += 1
-		else
-			hashtags[h]  = 1
-		end
+		tmp[h] = tmp[h] ? tmp[h] + 1 : 1
   end
 end
 
-hashtags.each { |h,val| puts "#{h}#{SEPARATOR}#{val}" }
+hashtags = tmp.sort_by { |h, count| -count }.
+               collect { |h, count| { hashtag: h, count: count } }
+
+puts JSON.pretty_generate(hashtags)
