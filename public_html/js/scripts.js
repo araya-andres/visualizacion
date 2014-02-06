a2d5177fa41b6859975d5fe7af6cc482b28bc9a8
Andres Araya <araya.andres@gmail.com>
2014-02-06 15:13:24 -0600

    typo.

--- public_html/js/scripts.js
+++ public_html/js/scripts.js
@@ -3,14 +3,18 @@ function draw(data) {
 	d3.select("body")
 		.append("div")
 			.attr("class", "chart")
-		.selectAll(".bar")
+		.selectAll("div.line")
 		.data(data)
 		.enter()
 		.append("div")
+			.attr("class", "line");
+	d3.selectAll("div.line")
+		.append("div")
+			.attr("class", "label")
+			.text( function (d) { return d.hashtag });
+	d3.selectAll("div.line")
+		.append("div")
 			.attr("class", "bar")
 			.style("width", function (d) { return d.count / 100 + "px" })
-			.style("outline", "1px solid black")
-			.text(function (d) {
-				return d.count > 20 ? d.hashtag : "";
-			});
+			.text(function (d) { return d.count; });
 }
