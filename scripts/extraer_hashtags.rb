1336cfaab6175d609ad542de6d62644db54e7b04
Andres Araya <araya.andres@gmail.com>
2014-02-06 05:44:20 -0600

    Refactoring

--- scripts/extraer_hashtags.rb
+++ scripts/extraer_hashtags.rb
@@ -4,14 +4,10 @@ file_name = ARGV[0]
 s = IO.read(file_name)
 tweets = JSON.parse(s)
 
-tmp = {}
-tweets["statuses"].each do |t|
-  t["new_hashtags"].each do |h|
-		tmp[h] = tmp[h] ? tmp[h] + 1 : 1
-  end
-end
-
-hashtags = tmp.sort_by { |h, count| -count }.
-               collect { |h, count| { hashtag: h, count: count } }
+hashtags = tweets["statuses"].inject({}) { |ans, t|
+  t["new_hashtags"].each { |h| ans[h] = ans[h] ? ans[h] + 1 : 1 }
+  ans
+}.sort_by { |h, count| -count }.
+  collect { |h, count| { hashtag: h, count: count } }
 
 puts JSON.pretty_generate(hashtags)
