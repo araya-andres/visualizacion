203a1a8090c4804c81957718c9ed92eab4e3550f
Andres Araya <araya.andres@gmail.com>
2014-02-07 11:14:22 -0600

    Tomar en cuenta los minutos donde no se generaron tweets.

--- scripts/tweets_por_minuto.rb
+++ scripts/tweets_por_minuto.rb
@@ -1,6 +1,7 @@
 require 'json'
 
-T0 = 1390176000 # 6:00 PM
+T0 = 1390176000 #  6:00 PM
+T1 = 1390197600 # 12:00 AM
 UN_MINUTO = 60
 
 file_name = ARGV[0]
@@ -8,11 +9,13 @@ s = IO.read(file_name)
 tweets = JSON.parse(s)
 
 intervalo = UN_MINUTO
-
-hashtags = tweets["statuses"].inject({}) { |ans, t|
-  i = ((t["created_at"] - T0) / intervalo) * intervalo + T0
-	ans[i] = 1 + (ans[i] || 0)
+hashtags = tweets["statuses"].inject(Array.new((T1 - T0) / intervalo, 0)) { |ans, t|
+  i = (t["created_at"] - T0) / intervalo
+	ans[i] += 1
   ans
-}.collect { |timestamp, count| { timestamp: timestamp, count: count } }
+}.each_with_index.collect { |count, i|
+  timestamp = i * intervalo + T0
+  { timestamp: timestamp, count: count }
+}
 
 puts JSON.pretty_generate(hashtags)
