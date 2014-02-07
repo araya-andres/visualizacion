74698320535ff31590bf192e850a613f3ce68570
Andres Araya <araya.andres@gmail.com>
2014-02-07 11:34:27 -0600

    Refactoring.

--- scripts/tweets_por_minuto.rb
+++ scripts/tweets_por_minuto.rb
@@ -4,18 +4,26 @@ T0 = 1390176000 #  6:00 PM
 T1 = 1390197600 # 12:00 AM
 UN_MINUTO = 60
 
+def indice(t, t0, intervalo)
+  (t - t0) / intervalo
+end
+
+def timestamp(i, t0, intervalo)
+  i * intervalo + t0
+end
+
 file_name = ARGV[0]
 s = IO.read(file_name)
 tweets = JSON.parse(s)
 
 intervalo = UN_MINUTO
-hashtags = tweets["statuses"].inject(Array.new((T1 - T0) / intervalo, 0)) { |ans, t|
-  i = (t["created_at"] - T0) / intervalo
-	ans[i] += 1
+n = indice(T1, T0, intervalo)
+hashtags = tweets["statuses"].inject(Array.new(n, 0)) { |ans, t|
+  i = indice(t["created_at"], T0, intervalo)
+  ans[i] += 1
   ans
 }.each_with_index.collect { |count, i|
-  timestamp = i * intervalo + T0
-  { timestamp: timestamp, count: count }
+  { timestamp: timestamp(i, T0, intervalo), count: count }
 }
 
 puts JSON.pretty_generate(hashtags)
