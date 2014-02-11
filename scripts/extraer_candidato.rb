a22f14e4d35dbbaca36a41881a373b351cebc957
Andres Araya <araya.andres@gmail.com>
2014-02-11 17:12:30 -0600

    Determinar si el mensaje corresponde al candidato, basado en un arreglo de expresiones regulares.

--- scripts/extraer_candidato.rb
+++ scripts/extraer_candidato.rb
@@ -4,14 +4,18 @@ file_name = ARGV[0]
 s = IO.read(file_name)
 tweets = JSON.parse(s)
 
+
+def match?(hashtags, re)
+  hashtags.each do |h|
+		re.each { |r| return true if r.match(h) }
+	end
+	return false
+end
+
+re = %w(johnny araya).map { |k| Regexp.new(k) }
 statuses = tweets["statuses"].inject([]) { |ans, t|
-  t["new_hashtags"].each { |h|
-		if h =~ /johnny/ ||
-			 h =~ /araya/
-			 ans << t
-		end
-	}
-  ans
+  ans << t if match?(t["new_hashtags"], re)
+	ans
 }
 tweets["statuses"] = statuses
 
