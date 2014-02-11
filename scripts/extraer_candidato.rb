f0849ba4f65daa82add40af1c6d9c2d25dd2e146
Andres Araya <araya.andres@gmail.com>
2014-02-11 17:18:01 -0600

    Formato (espaciado).

--- scripts/extraer_candidato.rb
+++ scripts/extraer_candidato.rb
@@ -7,15 +7,15 @@ tweets = JSON.parse(s)
 
 def match?(hashtags, re)
   hashtags.each do |h|
-		re.each { |r| return true if r.match(h) }
-	end
-	return false
+    re.each { |r| return true if r.match(h) }
+  end
+  return false
 end
 
 re = %w(johnny araya).map { |k| Regexp.new(k) }
 statuses = tweets["statuses"].inject([]) { |ans, t|
   ans << t if match?(t["new_hashtags"], re)
-	ans
+  ans
 }
 tweets["statuses"] = statuses
 
