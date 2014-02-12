ece77f44edda17ad8c6b3ef3be31b7de8f17892b
Andres Araya <araya.andres@gmail.com>
2014-02-12 06:12:25 -0600

    Refactoring.

--- scripts/extraer_candidato.rb
+++ scripts/extraer_candidato.rb
@@ -5,18 +5,21 @@ def read_tweets(file_name)
   JSON.parse(s)
 end
 
+def read_keywords(file_name)
+  IO.readlines(file_name).map { |k| Regexp.new(k.chomp) }
+end
+
 def match?(hashtags, re)
   hashtags.each do |h|
     re.each { |r| return true if r.match(h) }
   end
-  return false
+  false
 end
 
 tweets = read_tweets(ARGV[0])
-keywords = IO.readlines(ARGV[1])
-re = keywords.map { |k| Regexp.new(k.chomp) }
+keywords = read_keywords(ARGV[1])
 statuses = tweets["statuses"].inject([]) { |ans, t|
-  ans << t if match?(t["new_hashtags"], re)
+  ans << t if match?(t["new_hashtags"], keywords)
   ans
 }
 tweets["statuses"] = statuses
