02d56cd1f0f6e5d715d4195c14fce44eec226855
Andres Araya <araya.andres@gmail.com>
2014-02-11 18:12:48 -0600

    Prueba con Johnny Araya.

--- scripts/extraer_candidato.rb
+++ scripts/extraer_candidato.rb
@@ -4,6 +4,7 @@ file_name = ARGV[0]
 s = IO.read(file_name)
 tweets = JSON.parse(s)
 
+keywords=%w(john araya pln liberacion cambio responsable tiempo construir yoni jhon)
 
 def match?(hashtags, re)
   hashtags.each do |h|
@@ -12,7 +13,7 @@ def match?(hashtags, re)
   return false
 end
 
-re = %w(johnny araya).map { |k| Regexp.new(k) }
+re = keywords.map { |k| Regexp.new(k) }
 statuses = tweets["statuses"].inject([]) { |ans, t|
   ans << t if match?(t["new_hashtags"], re)
   ans
