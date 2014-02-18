979b0d759ffea2315ac8c7495b5d2a058302c3b2
Andres Araya <araya.andres@gmail.com>
2014-02-17 21:56:08 -0600

    Cambio de nombre de variables.

--- scripts/tweets_per_minute.rb
+++ scripts/tweets_per_minute.rb
@@ -4,10 +4,10 @@ load File.expand_path('common.rb', File.dirname(__FILE__))
 tweets = read_tweets(ARGV[0])
 interval = ONE_MINUTE
 n = index(T1, T0, interval)
-tweets["statuses"].inject(Array.new(n, 0)) { |tpm, t|
-  i = index(t["created_at"], T0, interval)
-  tpm[i] += 1
-  tpm
+tweets["statuses"].inject(Array.new(n, 0)) { |tweets_per_minute, tweet|
+  i = index(tweet["created_at"], T0, interval)
+  tweets_per_minute[i] += 1
+  tweets_per_minute
 }.each_with_index { |count, i|
   puts [timestamp_in_ms(i, T0, interval), count].to_csv
 }
