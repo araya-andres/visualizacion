79ced9cf82012707fb9987985da37e62f10f3335
Andres Araya <araya.andres@gmail.com>
2014-02-17 21:48:35 -0600

    Usando libraria estandar para CSV.

--- scripts/tweets_per_minute.rb
+++ scripts/tweets_per_minute.rb
@@ -1,12 +1,13 @@
+require 'csv'
 load File.expand_path('common.rb', File.dirname(__FILE__))
 
 tweets = read_tweets(ARGV[0])
 interval = ONE_MINUTE
 n = index(T1, T0, interval)
-tweets_per_minute = tweets["statuses"].inject(Array.new(n, 0)) { |tpm, t|
+tweets["statuses"].inject(Array.new(n, 0)) { |tpm, t|
   i = index(t["created_at"], T0, interval)
   tpm[i] += 1
   tpm
 }.each_with_index { |count, i|
-  puts "#{timestamp_in_ms(i, T0, interval)}#{CSV_SEPARATOR}#{count}"
+  puts [timestamp_in_ms(i, T0, interval), count].to_csv
 }
